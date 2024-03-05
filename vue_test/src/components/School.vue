ba47b7b319357886f8716ec032846c2290570fbd
malibo <1319529258@qq.com>
2024-03-05 21:36:58 +0800

    组件

---
+++ vue_test/src/components/School.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h3>学校名称：{{ schoolName }}</h3>
+        <h3>学校地址：{{ schoolAddress }}</h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'School',
+        data() {
+            return {
+                schoolName: '清华大学',
+                schoolAddress: '北京',
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
