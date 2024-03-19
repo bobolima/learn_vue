4310f66f346bafcc620c55ffc86bb1107a9f4202
malibo <1319529258@qq.com>
2024-03-19 22:22:54 +0800

    admin项目

---
+++ admin/src/main.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+// 引入element ui组件
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+// 引入axios请求组件
+import axios from 'axios'
+Vue.prototype.$axios = axios
+
+Vue.config.productionTip = false
+
+Vue.use(ElementUI)
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
---
+++ admin/src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home",
+        components: {},
+        props: {},
+        computed: {},
+        watch: {},
+        data() {
+            return {};
+        },
+        methods: {},
+        created() {
+        },
+        mounted() {
+        },
+        destroyed() {
+        }
+    };
+</script>
+
+<style scoped></style>
