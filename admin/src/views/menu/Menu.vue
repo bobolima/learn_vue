52d74430f29ab0e1643bcddcb9f8d3d67c418624
mlb <1319529258@qq.com>
2024-04-11 23:54:02 +0800

    菜单页面

--- admin/src/views/menu/Menu.vue
+++ admin/src/views/menu/Menu.vue
@@ -1,16 +1,130 @@
 <template>
-    <div>
-        菜单
-    </div>
+    <el-row>
+        <el-col :span="8">
+            <div class="grid-content bg-purple">
+                <el-input
+                    placeholder="输入关键字进行过滤"
+                    v-model="filterText">
+                </el-input>
+                <el-tree
+                    :data="data"
+                    node-key="id"
+                    default-expand-all
+                    @node-drag-start="handleDragStart"
+                    @node-drag-enter="handleDragEnter"
+                    @node-drag-leave="handleDragLeave"
+                    @node-drag-over="handleDragOver"
+                    @node-drag-end="handleDragEnd"
+                    @node-drop="handleDrop"
+                    draggable
+                    :allow-drop="allowDrop"
+                    :allow-drag="allowDrag"
+                    :filter-node-method="filterNode"
+                    ref="tree">
+                </el-tree>
+            </div>
+        </el-col>
+        <el-col :span="16"><div class="grid-content bg-purple-light">123</div></el-col>
+    </el-row>
+
 </template>
 
 <script>
     export default {
         name: "Menu",
         data() {
-            return {}
+            return {
+                filterText: '',
+                data: [{
+                    id: 1,
+                    label: '一级 1',
+                    children: [{
+                        id: 4,
+                        label: '二级 1-1',
+                        children: [{
+                            id: 9,
+                            label: '三级 1-1-1'
+                        }, {
+                            id: 10,
+                            label: '三级 1-1-2'
+                        }]
+                    }]
+                }, {
+                    id: 2,
+                    label: '一级 2',
+                    children: [{
+                        id: 5,
+                        label: '二级 2-1'
+                    }, {
+                        id: 6,
+                        label: '二级 2-2'
+                    }]
+                }, {
+                    id: 3,
+                    label: '一级 3',
+                    children: [{
+                        id: 7,
+                        label: '二级 3-1'
+                    }, {
+                        id: 8,
+                        label: '二级 3-2',
+                        children: [{
+                            id: 11,
+                            label: '三级 3-2-1'
+                        }, {
+                            id: 12,
+                            label: '三级 3-2-2'
+                        }, {
+                            id: 13,
+                            label: '三级 3-2-3'
+                        }]
+                    }]
+                }],
+                defaultProps: {
+                    children: 'children',
+                    label: 'label'
+                }
+            };
         },
-        methods: {}
+        watch: {
+            filterText(val) {
+                this.$refs.tree.filter(val);
+            }
+        },
+        methods: {
+            filterNode(value, data) {
+                if (!value) return true;
+                return data.label.indexOf(value) !== -1;
+            },
+            handleDragStart(node, ev) {
+                console.log('drag start', node, ev);
+            },
+            handleDragEnter(draggingNode, dropNode, ev) {
+                console.log('tree drag enter: ', dropNode.label, ev);
+            },
+            handleDragLeave(draggingNode, dropNode, ev) {
+                console.log('tree drag leave: ', dropNode.label, ev);
+            },
+            handleDragOver(draggingNode, dropNode, ev) {
+                console.log('tree drag over: ', dropNode.label, ev);
+            },
+            handleDragEnd(draggingNode, dropNode, dropType, ev) {
+                console.log('tree drag end: ', dropNode && dropNode.label, dropType, ev);
+            },
+            handleDrop(draggingNode, dropNode, dropType, ev) {
+                console.log('tree drop: ', dropNode.label, dropType, ev);
+            },
+            allowDrop(draggingNode, dropNode, type) {
+                if (dropNode.data.label === '二级 3-1') {
+                    return type !== 'inner';
+                } else {
+                    return true;
+                }
+            },
+            allowDrag(draggingNode) {
+                return draggingNode.data.label.indexOf('三级 3-2-2') === -1;
+            }
+        }
     }
 </script>
 
